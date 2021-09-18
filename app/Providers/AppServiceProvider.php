511e7f03c7ad069272e8f160aa08f132eacfbaec
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2021-09-18 14:07:06 +0100

    Project structure and dependency installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        Response::macro('success', function ($data, $status = 200) {
+            return response()->json([
+                'status_code' => $status,
+                'status' => 'success',
+                'data' => $data
+            ], $status);
+        });
+
+        Response::macro('error', function ($data, $message, $status = 400) {
+            return response()->json([
+                'status_code' => $status,
+                'status' => 'error',
+                'message' => $message
+            ], $status);
+        });
     }
 }
